03868b8733294ba3554a7ddf4b73bfee768e9801
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2021-08-03 07:48:47 +0530

    8. Fonts and Styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 //Components and pages
 import Home from "./pages/Home";
+import GlobalStyles from "./componenents/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
